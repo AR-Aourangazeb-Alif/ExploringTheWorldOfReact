c0814c25bbc30cfc9fbbe082d62bc229ea87830a
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-03-15 01:08:48 +0600

    integrating tailwind with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
       
+      <h1 className="text-center font-bold text-blue-400 text-xl p-8">Explore The World!</h1>
       
     </>
   )
