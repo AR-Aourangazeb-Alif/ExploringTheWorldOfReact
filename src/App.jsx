e048c03fee737b849012cc48a1aacbd28183d0dd
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-03-15 16:36:10 +0600

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Countries from "./country"
+
 function App() {
 
   return (
     <>
       
       <h1 className="text-center font-bold text-blue-400 text-xl p-8">Explore The World!</h1>
+
+      <Countries></Countries>
+      
       
     </>
   )
